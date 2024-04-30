70b00e42f9383bdf5a39141490b4f899f147bcfa
Ahmed ata abdallah <72627749+Ahmed-ata112@users.noreply.github.com>
2024-04-30 23:34:36 +0300

    relu forward and backward

--- nn-cuda/nn-cuda/relu.cu
+++ nn-cuda/nn-cuda/relu.cu
@@ -54,11 +54,31 @@ void relu_forward_cpu(float *input, float *output, int B, int N)
     }
 }
 
-void runKernel(float *input, float *output, int B, int N, int sqrt_block_size)
+void relu_backward_cpu(float *input, float *grad_output, float *grad_input, int B, int N)
+{
+    for (int i = 0; i < B; i++)
+    {
+        for (int j = N - 1; j >= 0; j--)
+        {
+            int idx = i * N + j;
+            grad_input[idx] = input[idx] > 0 ? grad_output[idx] : 0;
+        }
+    }
+}
+
+void runKernel1(float *input, float *output, int B, int N, int sqrt_block_size)
 {
     dim3 block(sqrt_block_size, sqrt_block_size);
     dim3 grid((B + block.x - 1) / block.x, (N + block.y - 1) / block.y);
     relu_forward<<<grid, block>>>(input, output, B, N);
+    cudaCheck(cudaDeviceSynchronize());
+}
+
+void runKernel2(float *input, float *grad_output, float *grad_input, int B, int N, int sqrt_block_size)
+{
+    dim3 block(sqrt_block_size, sqrt_block_size);
+    dim3 grid((B + block.x - 1) / block.x, (N + block.y - 1) / block.y);
+    relu_backward<<<grid, block>>>(input, grad_output, grad_input, B, N);
     cudaCheck(cudaDeviceSynchronize());
 }
 
@@ -72,23 +92,31 @@ int main()
 
     // Create host memory of random numbers
     float *out = (float *)malloc(B * N * sizeof(float));
+    float *grad_input = (float *)malloc(B * N * sizeof(float));
+    float *grad_output = make_random_float(B * N);
     float *inp = make_random_float(B * N);
 
 #if TEST_PYTORTH
-    write_npy("X_relu.npy", inp, 2, new unsigned long[2]{B, N});
+    write_npy("relu-layer\\X_relu.npy", inp, 2, new unsigned long[2]{B, N});
+    write_npy("relu-layer\\up_grad_relu.npy", grad_output, 2, new unsigned long[2]{B, N});
 #endif
 
     // Move to GPU
-    float *d_out;
-    float *d_inp;
+    float *d_out, *d_grad_input, *d_grad_output, *d_inp;
     cudaCheck(cudaMalloc(&d_out, B * N * sizeof(float)));
+    cudaCheck(cudaMalloc(&d_grad_input, B * N * sizeof(float)));
+    cudaCheck(cudaMalloc(&d_grad_output, B * N * sizeof(float)));
     cudaCheck(cudaMalloc(&d_inp, B * N * sizeof(float)));
+
     cudaCheck(cudaMemcpy(d_inp, inp, B * N * sizeof(float), cudaMemcpyHostToDevice));
+    cudaCheck(cudaMemcpy(d_grad_output, grad_output, B * N * sizeof(float), cudaMemcpyHostToDevice));
 
     relu_forward_cpu(inp, out, B, N);
+    relu_backward_cpu(inp, grad_output, grad_input, B, N);
 
 #if TEST_PYTORTH
-    write_npy("out_relu.npy", out, 2, new unsigned long[2]{B, N});
+    write_npy("relu-layer\\out_relu.npy", out, 2, new unsigned long[2]{B, N});
+    write_npy("relu-layer\\down_grad_relu.npy", grad_input, 2, new unsigned long[2]{B, N});
 #endif
 
     int sqrt_block_sizes[] = {4, 8, 16, 32};
@@ -97,8 +125,12 @@ int main()
     {
         int sqrt_block_size = sqrt_block_sizes[j];
         printf("Checking block size %d x %d.\n", sqrt_block_size, sqrt_block_size);
-        runKernel(d_inp, d_out, B, N, sqrt_block_size);
+
+        runKernel1(d_inp, d_out, B, N, sqrt_block_size);
         validate_result(d_out, out, "out", B * N, 1e-4f);
+
+        runKernel2(d_inp, d_grad_output, d_grad_input, B, N, sqrt_block_size);
+        validate_result(d_grad_input, grad_input, "grad_in", B * N, 1e-4f);
     }
 
     printf("All results match. Starting benchmarks.\n\n");
@@ -108,17 +140,26 @@ int main()
         int sqrt_block_size = sqrt_block_sizes[j];
 
         int repeat_times = 100;
-        float elapsed_time = benchmark_kernel(repeat_times, runKernel, d_inp, d_out, B, N, sqrt_block_size);
 
-        // Napkin math: estimate the flops achieved
-        float tflops = (float)B * N * 1 / elapsed_time * 1e3f / 1e12f;
-        printf("sqrt_block_size %4d | time %.4f ms | tflops %.2f\n", sqrt_block_size, elapsed_time, tflops);
+        float elapsed_time1 = benchmark_kernel(repeat_times, runKernel1, d_inp, d_out, B, N, sqrt_block_size);
+        float elapsed_time2 = benchmark_kernel(repeat_times, runKernel2, d_inp, d_grad_output, d_grad_input, B, N, sqrt_block_size);
+
+        float tflops1 = (float)B * N * 1 / elapsed_time1 * 1e3f / 1e12f;
+        float tflops2 = (float)B * N * 1 / elapsed_time2 * 1e3f / 1e12f;
+
+        printf("Forward pass: sqrt_block_size %4d | time %.4f ms | tflops %.2f\n", sqrt_block_size, elapsed_time1, tflops1);
+        printf("Backward pass: sqrt_block_size %4d | time %.4f ms | tflops %.2f\n", sqrt_block_size, elapsed_time2, tflops2);
     }
 
     free(out);
     free(inp);
+    free(grad_input);
+    free(grad_output);
+
     cudaCheck(cudaFree(d_out));
     cudaCheck(cudaFree(d_inp));
+    cudaCheck(cudaFree(d_grad_input));
+    cudaCheck(cudaFree(d_grad_output));
 
     return 0;
 }
