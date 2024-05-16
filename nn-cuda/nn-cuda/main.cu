af6addbd862d8c835550557e56acf79e089f8384
Fady Adel <75928317+fady2001@users.noreply.github.com>
2024-05-16 23:20:20 +0300

    .

--- nn-cuda/nn-cuda/main.cu
+++ nn-cuda/nn-cuda/main.cu
@@ -5,124 +5,99 @@
 #include <cmath>
 #define TEST_PYTORTH true
 
-void get_from_gpu_and_print(const char *name, float *d_ptr, int size) {
-  float *ptr = (float *)malloc(size * sizeof(float));
-  cudaCheck(
-      cudaMemcpy(ptr, d_ptr, size * sizeof(float), cudaMemcpyDeviceToHost));
-  printf("%s\n", name);
-  for (int i = 0; i < 10; i++) {
-    printf("%f\n", ptr[i]);
-  }
-  printf("-----------------------\n");
-  free(ptr);
-}
-
 int main() {
-  uint input_dim = 16;
-  uint B = 16;
-  uint H1 = 512;
-  uint C = 32;
-  ModelMemoryHandler h_model(input_dim, B, H1, C, RANDOM_V, RANDOM_V);
+	uint input_dim = 16;
+	uint B = 16;
+	uint H1 = 16;
+	uint C = 16;
+	ModelMemoryHandler h_model(input_dim, B, H1, C, RANDOM_V, RANDOM_V);
 
-  // create host memory of random numbers
-  float *inp = make_random_float(B * input_dim);
-  int *target = make_random_int(B, int(C));
+	// create host memory of random numbers
+	float* inp = make_random_float(B * input_dim);
+	int* target = make_random_int(B, int(C));
 
 #if TEST_PYTORTH
-  write_npy("all-model\\X_c.npy", inp, 2, new unsigned long[2]{B, input_dim});
-  write_npy("all-model\\target.npy", target, 1, new unsigned long[1]{B});
-  write_npy("all-model\\ln1w.npy", h_model.GetParams().ln1w, 2,
-            new unsigned long[2]{H1, input_dim});
-  write_npy("all-model\\ln1b.npy", h_model.GetParams().ln1b, 1,
-            new unsigned long[1]{H1});
-  write_npy("all-model\\ln2w.npy", h_model.GetParams().ln2w, 2,
-            new unsigned long[2]{C, H1});
-  write_npy("all-model\\ln2b.npy", h_model.GetParams().ln2b, 1,
-            new unsigned long[1]{C});
+	write_npy("all-model\\X_c.npy", inp, 2, new unsigned long[2]{ B, input_dim });
+	write_npy("all-model\\target.npy", target, 1, new unsigned long[1]{ B });
+	write_npy("all-model\\ln1w.npy", h_model.GetParams().ln1w, 2, new unsigned long[2]{ H1, input_dim });
+	write_npy("all-model\\ln1b.npy", h_model.GetParams().ln1b, 1, new unsigned long[1]{ H1 });
+	write_npy("all-model\\ln2w.npy", h_model.GetParams().ln2w, 2, new unsigned long[2]{ C, H1 });
+	write_npy("all-model\\ln2b.npy", h_model.GetParams().ln2b, 1, new unsigned long[1]{ C });
 #endif
 
-  int deviceIdx = 0;
-  cudaCheck(cudaSetDevice(deviceIdx));
+	int deviceIdx = 0;
+	cudaCheck(cudaSetDevice(deviceIdx));
 
-  // move to GPU
-  ModelMemoryHandler d_model;
-  h_model.model_to_cuda(&d_model);
+	// move to GPU
+	ModelMemoryHandler d_model;
+	h_model.model_to_cuda(&d_model);
 
-  // move input and target to GPU
-  float *d_inp;
-  uint *d_target;
-  cudaCheck(cudaMalloc(&d_inp, B * input_dim * sizeof(float)));
-  cudaCheck(cudaMalloc(&d_target, B * sizeof(uint)));
-  cudaCheck(cudaMemcpy(d_inp, inp, B * input_dim * sizeof(float),
-                       cudaMemcpyHostToDevice));
-  cudaCheck(
-      cudaMemcpy(d_target, target, B * sizeof(int), cudaMemcpyHostToDevice));
+	// move input and target to GPU
+	float* d_inp;
+	uint* d_target;
+	cudaCheck(cudaMalloc(&d_inp, B * input_dim * sizeof(float)));
+	cudaCheck(cudaMalloc(&d_target, B * sizeof(uint)));
+	cudaCheck(cudaMemcpy(d_inp, inp, B * input_dim * sizeof(float),cudaMemcpyHostToDevice));
+	cudaCheck(cudaMemcpy(d_target, target, B * sizeof(int), cudaMemcpyHostToDevice));
 
-  // run the model
-  ModelLayersKernelsLaunchers::linear_layer(
-      d_inp, d_model.GetParams().ln1w, d_model.GetParams().ln1b,
-      d_model.GetActivations().ln1, B, input_dim, H1, 32);
-  ModelLayersKernelsLaunchers::run_relu_kernel(
-      d_model.GetActivations().ln1, d_model.GetActivations().a1, B, H1, 32);
-  ModelLayersKernelsLaunchers::linear_layer(
-      d_model.GetActivations().a1, d_model.GetParams().ln2w,
-      d_model.GetParams().ln2b, d_model.GetActivations().ln2, B, H1, C, 32);
-  ModelLayersKernelsLaunchers::run_softmax_kernel(
-      d_model.GetActivations().ln2, d_model.GetActivations().sm, B, C, 32);
-  ModelLayersKernelsLaunchers::run_cross_entropy_kernel(
-      d_model.GetActivations().loss, d_model.GetActivations().sm, d_target, B,
-      C, 32);
-  ModelLayersKernelsLaunchers::run_array_sum_kernel3(
-      d_model.GetActivations().loss, d_model.GetActivations().reduced_loss, B,
-      32);
+	// run the model
+	ModelLayersKernelsLaunchers::linear_layer(d_inp, d_model.GetParams().ln1w, d_model.GetParams().ln1b,d_model.GetActivations().ln1, B, input_dim, H1, 32);
+	save_2d("ln1", d_model.GetActivations().ln1, B, H1);
+	ModelLayersKernelsLaunchers::run_relu_kernel(d_model.GetActivations().ln1, d_model.GetActivations().a1, B, H1, 32);
+	save_2d("a1", d_model.GetActivations().a1, B, H1);
+	ModelLayersKernelsLaunchers::linear_layer(d_model.GetActivations().a1, d_model.GetParams().ln2w,d_model.GetParams().ln2b, d_model.GetActivations().ln2, B, H1, C, 32);
+	save_2d("ln2", d_model.GetActivations().ln2, B, C);
+	ModelLayersKernelsLaunchers::run_softmax_kernel(d_model.GetActivations().ln2, d_model.GetActivations().sm, B, C, 32);
+	save_2d("sm", d_model.GetActivations().sm, B, C);
+	ModelLayersKernelsLaunchers::run_cross_entropy_kernel(d_model.GetActivations().loss, d_model.GetActivations().sm, d_target, B, C, 32);
+	save_1d("loss", d_model.GetActivations().loss, B);
+	ModelLayersKernelsLaunchers::run_array_sum_kernel3(d_model.GetActivations().loss, d_model.GetActivations().reduced_loss, B,32);
 
-  // cuda synchronize();
-  cudaCheck(cudaDeviceSynchronize());
-  // copy the loss to the host
-  float *reduced_loss = (float *)malloc(sizeof(float));
-  cudaCheck(cudaMemcpy(reduced_loss, d_model.GetActivations().reduced_loss,
-                       sizeof(float), cudaMemcpyDeviceToHost));
-  printf("Loss: %f\n", *reduced_loss);
+	// cuda synchronize();
+	cudaCheck(cudaDeviceSynchronize());
+	// copy the loss to the host
+	float* reduced_loss = (float*)malloc(sizeof(float));
+	cudaCheck(cudaMemcpy(reduced_loss, d_model.GetActivations().reduced_loss,
+		sizeof(float), cudaMemcpyDeviceToHost));
+	printf("Loss: %f\n", *reduced_loss);
 
-  // backpropagation
-  ModelLayersKernelsLaunchers::run_crossentropy_softmax_backward(
-      d_model.GetDownstreamGradients().dsm, d_model.GetActivations().sm,
-      d_target, B, C, 32);
-  get_from_gpu_and_print("dsm", d_model.GetDownstreamGradients().dsm, B * C);
+	// backpropagation
+	ModelLayersKernelsLaunchers::run_crossentropy_softmax_backward(
+		d_model.GetDownstreamGradients().dsm, d_model.GetActivations().sm,
+		d_target, B, C, 32);
+	float *dsm = (float*)malloc(B * C * sizeof(float));
+	cudaCheck(cudaMemcpy(dsm, d_model.GetDownstreamGradients().dsm, B * C * sizeof(float), cudaMemcpyDeviceToHost));
+	#if TEST_PYTORTH
+		write_npy("all-model\\dsm.npy", dsm, 2, new unsigned long[2]{ B, C });
+	#endif
+	// get_from_gpu_and_print("dsm", d_model.GetDownstreamGradients().dsm, B * C);
 
-  ModelLayersKernelsLaunchers::runLinearBackward(
-      d_model.GetActivations().a1, d_model.GetParams().ln2w,
-      d_model.GetDownstreamGradients().dsm, d_model.GetGradients().ln2w_grad,
-      d_model.GetGradients().ln2b_grad, d_model.GetDownstreamGradients().dln2,
-      B, H1, C, 32);
+	ModelLayersKernelsLaunchers::runLinearBackward(
+		d_model.GetActivations().a1, d_model.GetParams().ln2w,
+		d_model.GetDownstreamGradients().dsm, d_model.GetGradients().ln2w_grad,
+		d_model.GetGradients().ln2b_grad, d_model.GetDownstreamGradients().dln2,
+		B, H1, C, 32);
+	save_2d("dln2", d_model.GetDownstreamGradients().dln2, B, H1);
+	save_2d("ln2w_grad", d_model.GetGradients().ln2w_grad, C, H1);
+	save_1d("ln2b_grad", d_model.GetGradients().ln2b_grad, C);
+	
+	// get_from_gpu_and_print("dln2", d_model.GetDownstreamGradients().dln2, B * H1);
+	// get_from_gpu_and_print("ln2w_grad", d_model.GetGradients().ln2w_grad, C * H1);
+	// get_from_gpu_and_print("ln2b_grad", d_model.GetGradients().ln2b_grad, C);
 
-  get_from_gpu_and_print("dln2", d_model.GetDownstreamGradients().dln2, B * H1);
-  get_from_gpu_and_print("ln2w_grad", d_model.GetGradients().ln2w_grad, C * H1);
-  get_from_gpu_and_print("ln2b_grad", d_model.GetGradients().ln2b_grad, C);
-  ModelLayersKernelsLaunchers::runReluBackward(
-      d_model.GetActivations().ln1, d_model.GetDownstreamGradients().dln2,
-      d_model.GetDownstreamGradients().da1, B, H1, 32);
-
-  get_from_gpu_and_print("da1", d_model.GetDownstreamGradients().da1, B * H1);
-  ModelLayersKernelsLaunchers::runLinearBackward(
-      d_inp, d_model.GetParams().ln1w, d_model.GetDownstreamGradients().da1,
-      d_model.GetGradients().ln1w_grad, d_model.GetGradients().ln1b_grad,
-      d_model.GetDownstreamGradients().dln1, B, input_dim, H1, 32);
-
-  get_from_gpu_and_print("dln1", d_model.GetDownstreamGradients().dln1,
-                         B * input_dim);
-  //  for (int i = 0; i < 10; i++) {
-  //    printf("%f\n", dln1_grad[i]);
-  //  }
-  //  printf("-----------------------\n");
-
-  // print first few gradients
-  //  for (int i = 0; i < 10; i++) {
-  //    printf("%f\n", ln1w_grad[i]);
-  //  }
-  //  printf("-----------------------\n");
-  //  for (int i = 0; i < 10; i++) {
-  //    printf("%f\n", ln1b_grad[i]);
-  //  }
-  return 0;
+	ModelLayersKernelsLaunchers::runReluBackward(
+		d_model.GetActivations().ln1, d_model.GetDownstreamGradients().dln2,
+		d_model.GetDownstreamGradients().da1, B, H1, 32);
+	save_2d("da1", d_model.GetDownstreamGradients().da1, B, H1);
+	// get_from_gpu_and_print("da1", d_model.GetDownstreamGradients().da1, B * H1);
+	ModelLayersKernelsLaunchers::runLinearBackward(
+		d_inp, d_model.GetParams().ln1w, d_model.GetDownstreamGradients().da1,
+		d_model.GetGradients().ln1w_grad, d_model.GetGradients().ln1b_grad,
+		d_model.GetDownstreamGradients().dln1, B, input_dim, H1, 32);
+	save_2d("dln1", d_model.GetDownstreamGradients().dln1, B, input_dim);
+	save_2d("ln1w_grad", d_model.GetGradients().ln1w_grad, H1, input_dim);
+	save_1d("ln1b_grad", d_model.GetGradients().ln1b_grad, H1);
+	// get_from_gpu_and_print("dln1", d_model.GetDownstreamGradients().dln1,B * input_dim);
+	
+	return 0;
 }
--- nn-cuda/nn-cuda/relu.cu
+++ nn-cuda/nn-cuda/relu.cu
@@ -1,172 +1,172 @@
-#include "common.hpp"
-#define TEST_PYTORTH true
-
-/**
- * @brief
- *  This function performs the forward pass of a ReLU activation function.
- *
- * @param input: Input tensor of shape (B, N) where B is the batch size and N is
- * the number of elements per batch.
- * @param output: Output tensor of the same shape as the input.
- */
-__global__ void relu_forward(float *input, float *output, int B, int N) {
-  // This maps one thread to one element in the input.
-  int i = blockIdx.x * blockDim.x + threadIdx.x;
-  int j = blockIdx.y * blockDim.y + threadIdx.y;
-
-  if (i < B && j < N) {
-    int idx = i * N + j;
-    output[idx] = fmaxf(0.0f, input[idx]);
-  }
-}
-void relu_forward_cpu(float *input, float *output, int B, int N) {
-  for (int i = 0; i < B; i++) {
-    for (int j = 0; j < N; j++) {
-      int idx = i * N + j;
-      output[idx] = fmaxf(0.0f, input[idx]);
-    }
-  }
-}
-/**
- * @brief
- *  This function performs the backward pass of a ReLU activation function.
- *
- * @param input: Input tensor of shape (B, N) from the forward pass.
- * @param grad_output: Gradient tensor from the next layer.
- * @param grad_input: Gradient tensor to propagate back.
- */
-__global__ void relu_backward(float *input, float *up_grad, float *down_grad,
-                              int B, int N) {
-  // This maps one thread to one element in the input.
-  int i = blockIdx.x * blockDim.x + threadIdx.x;
-  int j = blockIdx.y * blockDim.y + threadIdx.y;
-
-  if (i < B && j < N) {
-    int idx = i * N + j;
-    down_grad[idx] = input[idx] > 0 ? up_grad[idx] : 0;
-  }
-}
-
-void relu_backward_cpu(float *input, float *grad_output, float *grad_input,
-                       int B, int N) {
-  for (int i = 0; i < B; i++) {
-    for (int j = N - 1; j >= 0; j--) {
-      int idx = i * N + j;
-      grad_input[idx] = input[idx] > 0 ? grad_output[idx] : 0;
-    }
-  }
-}
-
-void run_relu_kernel(float *input, float *output, int B, int N,
-                     int sqrt_block_size) {
-  dim3 block(sqrt_block_size, sqrt_block_size);
-  dim3 grid((B + block.x - 1) / block.x, (N + block.y - 1) / block.y);
-  relu_forward<<<grid, block>>>(input, output, B, N);
-  cudaCheck(cudaDeviceSynchronize());
-}
-
-void runKernel2(float *input, float *grad_output, float *grad_input, int B,
-                int N, int sqrt_block_size) {
-  dim3 block(sqrt_block_size, sqrt_block_size);
-  dim3 grid((B + block.x - 1) / block.x, (N + block.y - 1) / block.y);
-  relu_backward<<<grid, block>>>(input, grad_output, grad_input, B, N);
-  cudaCheck(cudaDeviceSynchronize());
-}
-
-void runReluBackward(float *input, float *grad_output, float *grad_input, int B,
-                     int N, int sqrt_block_size) {
-  dim3 block(sqrt_block_size, sqrt_block_size);
-  dim3 grid((B + block.x - 1) / block.x, (N + block.y - 1) / block.y);
-  relu_backward<<<grid, block>>>(input, grad_output, grad_input, B, N);
-  cudaCheck(cudaDeviceSynchronize());
-}
-
-int main() {
-  srand(0);
-  const unsigned long B = 100, N = 100;
-
-  int deviceIdx = 0;
-  cudaCheck(cudaSetDevice(deviceIdx));
-
-  // Create host memory of random numbers
-  float *out = (float *)malloc(B * N * sizeof(float));
-  float *grad_input = (float *)malloc(B * N * sizeof(float));
-  float *grad_output = make_random_float(B * N);
-  float *inp = make_random_float(B * N);
-
-#if TEST_PYTORTH
-  write_npy("relu-layer\\X_relu.npy", inp, 2,
-            reinterpret_cast<const unsigned long *>(new size_t[2]{B, N}));
-  write_npy("relu-layer\\up_grad_relu.npy", grad_output, 2,
-            reinterpret_cast<const unsigned long *>(new size_t[2]{B, N}));
-#endif
-
-  // Move to GPU
-  float *d_out, *d_grad_input, *d_grad_output, *d_inp;
-  cudaCheck(cudaMalloc(&d_out, B * N * sizeof(float)));
-  cudaCheck(cudaMalloc(&d_grad_input, B * N * sizeof(float)));
-  cudaCheck(cudaMalloc(&d_grad_output, B * N * sizeof(float)));
-  cudaCheck(cudaMalloc(&d_inp, B * N * sizeof(float)));
-
-  cudaCheck(
-      cudaMemcpy(d_inp, inp, B * N * sizeof(float), cudaMemcpyHostToDevice));
-  cudaCheck(cudaMemcpy(d_grad_output, grad_output, B * N * sizeof(float),
-                       cudaMemcpyHostToDevice));
-
-  relu_forward_cpu(inp, out, B, N);
-  relu_backward_cpu(inp, grad_output, grad_input, B, N);
-
-#if TEST_PYTORTH
-  write_npy("relu-layer\\out_relu.npy", out, 2,
-            reinterpret_cast<const unsigned long *>(new size_t[2]{B, N}));
-  write_npy("relu-layer\\down_grad_relu.npy", grad_input, 2,
-            reinterpret_cast<const unsigned long *>(new size_t[2]{B, N}));
-#endif
-
-  int sqrt_block_sizes[] = {4, 8, 16, 32};
-
-  for (int j = 0; j < sizeof(sqrt_block_sizes) / sizeof(int); j++) {
-    int sqrt_block_size = sqrt_block_sizes[j];
-    printf("Checking block size %d x %d.\n", sqrt_block_size, sqrt_block_size);
-
-    run_relu_kernel(d_inp, d_out, B, N, sqrt_block_size);
-    validate_result(d_out, out, "out", B * N, 1e-4f);
-
-    runKernel2(d_inp, d_grad_output, d_grad_input, B, N, sqrt_block_size);
-    validate_result(d_grad_input, grad_input, "grad_in", B * N, 1e-4f);
-  }
-
-  printf("All results match. Starting benchmarks.\n\n");
-
-  for (int j = 0; j < sizeof(sqrt_block_sizes) / sizeof(int); j++) {
-    int sqrt_block_size = sqrt_block_sizes[j];
-
-    int repeat_times = 100;
-
-    float elapsed_time1 = benchmark_kernel(repeat_times, run_relu_kernel, d_inp,
-                                           d_out, B, N, sqrt_block_size);
-    float elapsed_time2 =
-        benchmark_kernel(repeat_times, runKernel2, d_inp, d_grad_output,
-                         d_grad_input, B, N, sqrt_block_size);
-
-    float tflops1 = (float)B * N * 1 / elapsed_time1 * 1e3f / 1e12f;
-    float tflops2 = (float)B * N * 1 / elapsed_time2 * 1e3f / 1e12f;
-
-    printf("Forward pass: sqrt_block_size %4d | time %.4f ms | tflops %.2f\n",
-           sqrt_block_size, elapsed_time1, tflops1);
-    printf("Backward pass: sqrt_block_size %4d | time %.4f ms | tflops %.2f\n",
-           sqrt_block_size, elapsed_time2, tflops2);
-  }
-
-  free(out);
-  free(inp);
-  free(grad_input);
-  free(grad_output);
-
-  cudaCheck(cudaFree(d_out));
-  cudaCheck(cudaFree(d_inp));
-  cudaCheck(cudaFree(d_grad_input));
-  cudaCheck(cudaFree(d_grad_output));
-
-  return 0;
-}
+//#include "common.hpp"
+//#define TEST_PYTORTH true
+//
+///**
+// * @brief
+// *  This function performs the forward pass of a ReLU activation function.
+// *
+// * @param input: Input tensor of shape (B, N) where B is the batch size and N is
+// * the number of elements per batch.
+// * @param output: Output tensor of the same shape as the input.
+// */
+//__global__ void relu_forward(float *input, float *output, int B, int N) {
+//  // This maps one thread to one element in the input.
+//  int i = blockIdx.x * blockDim.x + threadIdx.x;
+//  int j = blockIdx.y * blockDim.y + threadIdx.y;
+//
+//  if (i < B && j < N) {
+//    int idx = i * N + j;
+//    output[idx] = fmaxf(0.0f, input[idx]);
+//  }
+//}
+//void relu_forward_cpu(float *input, float *output, int B, int N) {
+//  for (int i = 0; i < B; i++) {
+//    for (int j = 0; j < N; j++) {
+//      int idx = i * N + j;
+//      output[idx] = fmaxf(0.0f, input[idx]);
+//    }
+//  }
+//}
+///**
+// * @brief
+// *  This function performs the backward pass of a ReLU activation function.
+// *
+// * @param input: Input tensor of shape (B, N) from the forward pass.
+// * @param grad_output: Gradient tensor from the next layer.
+// * @param grad_input: Gradient tensor to propagate back.
+// */
+//__global__ void relu_backward(float *input, float *up_grad, float *down_grad,
+//                              int B, int N) {
+//  // This maps one thread to one element in the input.
+//  int i = blockIdx.x * blockDim.x + threadIdx.x;
+//  int j = blockIdx.y * blockDim.y + threadIdx.y;
+//
+//  if (i < B && j < N) {
+//    int idx = i * N + j;
+//    down_grad[idx] = input[idx] > 0 ? up_grad[idx] : 0;
+//  }
+//}
+//
+//void relu_backward_cpu(float *input, float *grad_output, float *grad_input,
+//                       int B, int N) {
+//  for (int i = 0; i < B; i++) {
+//    for (int j = N - 1; j >= 0; j--) {
+//      int idx = i * N + j;
+//      grad_input[idx] = input[idx] > 0 ? grad_output[idx] : 0;
+//    }
+//  }
+//}
+//
+//void run_relu_kernel(float *input, float *output, int B, int N,
+//                     int sqrt_block_size) {
+//  dim3 block(sqrt_block_size, sqrt_block_size);
+//  dim3 grid((B + block.x - 1) / block.x, (N + block.y - 1) / block.y);
+//  relu_forward<<<grid, block>>>(input, output, B, N);
+//  cudaCheck(cudaDeviceSynchronize());
+//}
+//
+//void runKernel2(float *input, float *grad_output, float *grad_input, int B,
+//                int N, int sqrt_block_size) {
+//  dim3 block(sqrt_block_size, sqrt_block_size);
+//  dim3 grid((B + block.x - 1) / block.x, (N + block.y - 1) / block.y);
+//  relu_backward<<<grid, block>>>(input, grad_output, grad_input, B, N);
+//  cudaCheck(cudaDeviceSynchronize());
+//}
+//
+//void runReluBackward(float *input, float *grad_output, float *grad_input, int B,
+//                     int N, int sqrt_block_size) {
+//  dim3 block(sqrt_block_size, sqrt_block_size);
+//  dim3 grid((B + block.x - 1) / block.x, (N + block.y - 1) / block.y);
+//  relu_backward<<<grid, block>>>(input, grad_output, grad_input, B, N);
+//  cudaCheck(cudaDeviceSynchronize());
+//}
+//
+//int main() {
+//  srand(0);
+//  const unsigned long B = 100, N = 100;
+//
+//  int deviceIdx = 0;
+//  cudaCheck(cudaSetDevice(deviceIdx));
+//
+//  // Create host memory of random numbers
+//  float *out = (float *)malloc(B * N * sizeof(float));
+//  float *grad_input = (float *)malloc(B * N * sizeof(float));
+//  float *grad_output = make_random_float(B * N);
+//  float *inp = make_random_float(B * N);
+//
+//#if TEST_PYTORTH
+//  write_npy("relu-layer\\X_relu.npy", inp, 2,
+//            reinterpret_cast<const unsigned long *>(new size_t[2]{B, N}));
+//  write_npy("relu-layer\\up_grad_relu.npy", grad_output, 2,
+//            reinterpret_cast<const unsigned long *>(new size_t[2]{B, N}));
+//#endif
+//
+//  // Move to GPU
+//  float *d_out, *d_grad_input, *d_grad_output, *d_inp;
+//  cudaCheck(cudaMalloc(&d_out, B * N * sizeof(float)));
+//  cudaCheck(cudaMalloc(&d_grad_input, B * N * sizeof(float)));
+//  cudaCheck(cudaMalloc(&d_grad_output, B * N * sizeof(float)));
+//  cudaCheck(cudaMalloc(&d_inp, B * N * sizeof(float)));
+//
+//  cudaCheck(
+//      cudaMemcpy(d_inp, inp, B * N * sizeof(float), cudaMemcpyHostToDevice));
+//  cudaCheck(cudaMemcpy(d_grad_output, grad_output, B * N * sizeof(float),
+//                       cudaMemcpyHostToDevice));
+//
+//  relu_forward_cpu(inp, out, B, N);
+//  relu_backward_cpu(inp, grad_output, grad_input, B, N);
+//
+//#if TEST_PYTORTH
+//  write_npy("relu-layer\\out_relu.npy", out, 2,
+//            reinterpret_cast<const unsigned long *>(new size_t[2]{B, N}));
+//  write_npy("relu-layer\\down_grad_relu.npy", grad_input, 2,
+//            reinterpret_cast<const unsigned long *>(new size_t[2]{B, N}));
+//#endif
+//
+//  int sqrt_block_sizes[] = {4, 8, 16, 32};
+//
+//  for (int j = 0; j < sizeof(sqrt_block_sizes) / sizeof(int); j++) {
+//    int sqrt_block_size = sqrt_block_sizes[j];
+//    printf("Checking block size %d x %d.\n", sqrt_block_size, sqrt_block_size);
+//
+//    run_relu_kernel(d_inp, d_out, B, N, sqrt_block_size);
+//    validate_result(d_out, out, "out", B * N, 1e-4f);
+//
+//    runKernel2(d_inp, d_grad_output, d_grad_input, B, N, sqrt_block_size);
+//    validate_result(d_grad_input, grad_input, "grad_in", B * N, 1e-4f);
+//  }
+//
+//  printf("All results match. Starting benchmarks.\n\n");
+//
+//  for (int j = 0; j < sizeof(sqrt_block_sizes) / sizeof(int); j++) {
+//    int sqrt_block_size = sqrt_block_sizes[j];
+//
+//    int repeat_times = 100;
+//
+//    float elapsed_time1 = benchmark_kernel(repeat_times, run_relu_kernel, d_inp,
+//                                           d_out, B, N, sqrt_block_size);
+//    float elapsed_time2 =
+//        benchmark_kernel(repeat_times, runKernel2, d_inp, d_grad_output,
+//                         d_grad_input, B, N, sqrt_block_size);
+//
+//    float tflops1 = (float)B * N * 1 / elapsed_time1 * 1e3f / 1e12f;
+//    float tflops2 = (float)B * N * 1 / elapsed_time2 * 1e3f / 1e12f;
+//
+//    printf("Forward pass: sqrt_block_size %4d | time %.4f ms | tflops %.2f\n",
+//           sqrt_block_size, elapsed_time1, tflops1);
+//    printf("Backward pass: sqrt_block_size %4d | time %.4f ms | tflops %.2f\n",
+//           sqrt_block_size, elapsed_time2, tflops2);
+//  }
+//
+//  free(out);
+//  free(inp);
+//  free(grad_input);
+//  free(grad_output);
+//
+//  cudaCheck(cudaFree(d_out));
+//  cudaCheck(cudaFree(d_inp));
+//  cudaCheck(cudaFree(d_grad_input));
+//  cudaCheck(cudaFree(d_grad_output));
+//
+//  return 0;
+//}
